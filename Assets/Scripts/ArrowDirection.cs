2ed98d5ed431e94c005f67eb7df3e261829fbe29
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2019-12-11 13:35:42 +0400

    Initial commit

---
+++ Assets/Scripts/ArrowDirection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+// Attach this script to the camera that you want to follow the target
+public class ArrowDirection : MonoBehaviour
+{
+    public Quaternion targetRot;
+    public Transform arrow;
+    private readonly float rotationSmoothingSpeed = 2.0f;
+
+    void LateUpdate()
+    {
+        Vector3 targetEulerAngles = targetRot.eulerAngles;
+        float rotationToApplyAroundY = targetEulerAngles.y ;
+        float newCamRotAngleY = Mathf.LerpAngle(transform.eulerAngles.y,
+        rotationToApplyAroundY, rotationSmoothingSpeed * Time.deltaTime);
+        arrow.LookAt(arrow);
+        
+    }
+}
